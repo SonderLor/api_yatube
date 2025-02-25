d77995044ae8ba8e122baf19c74ffa48aa4c9d18
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-26 02:15:33 +0300

    fix url routers

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,24 +6,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet, basename='posts')
 router.register(r'groups', views.GroupViewSet, basename='groups')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    views.CommentViewSet, basename='comments'
+)
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('', include(router.urls)),
-
-    re_path(
-        r'^posts/(?P<post_id>\d+)/comments/$',
-        views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
-        name='post-comments-list'
-    ),
-    re_path(
-        r'^posts/(?P<post_id>\d+)/comments/(?P<pk>\d+)/$',
-        views.CommentViewSet.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'patch': 'partial_update',
-            'delete': 'destroy'
-        }),
-        name='post-comment-detail'
-    ),
 ]
